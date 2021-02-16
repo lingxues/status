151973d39d5f899070fa4968786eaaa1f5fee8c9
LingxueFreeLab <60049975+LingxueFreeLab@users.noreply.github.com>
2021-02-16 15:23:13 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// array of Monitor-specific API keys or Read-Only API keys to list all monitors
+var __apiKeys = [
+	'm785527004-5b7e61883f2c5bac07736d3d', //host
+	'm785527085-9ef9584911cfaaee94db1ee0', //gongji
+	'm787252657-c29e97123f495fb072f39767', //lingxues
+	'm787238094-d59bde6ae3aa581915051fab', //dns
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
